85713f2d45a78c63061235d549a76ed7ebe00415
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-10-17 17:17:59 +0200

    Create index.cpp

---
+++ C'est plus, c'est moins/index.cpp
@@ -0,0 +1,43 @@
+int a, b=-1, n= 0;
+
+void setup() {
+
+  randomSeed(analogRead(A0));
+  a = random(0, 100); // nombre aléatoire entre 0 et 100
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  while(a != b) {
+
+    b = lirenombre("Saisir un nombre : ");
+    n = ???;
+    if (b???a) Serial.println(" Trop grand");
+    if (b???a) Serial.println(" Trop petit");
+
+  }
+
+  Serial.print(" Gagne en ");
+  Serial.print(n);
+  Serial.println(" coups");
+  while(1);
+}
+
+// La fonction ci-dessous permet de lire un nombre via le moniteur série
+int lirenombre(String message) {
+
+  String saisie = "";
+  Serial.print(message);
+  while (Serial.available() == 0);
+  while (Serial.available() > 0) {
+
+    char caractere = Serial.read();
+    delay(1);
+    if(isDigit(caractere)) saisie = saisie + caractere; 
+  }
+
+  int nombre = saisie.toInt();
+  Serial.print(nombre);
+  return nombre;
+}
