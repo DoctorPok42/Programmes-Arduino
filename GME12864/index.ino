d513527e05ae8c2dc08c6fe03e345e72649434ea
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2021-11-21 15:09:59 +0100

    Update index.ino

--- GME12864/index.ino
+++ GME12864/index.ino
@@ -1,26 +1,29 @@
-#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-Servo barriere;
-int bp = 2; // Le bouton qui remplace le lecteur de badge
-int LDR = A0;
-const int ouvre=90;
-const int ferme=0;
+#define SCREEN_WIDTH 128 // La longueur de l'écran, en pixels
+#define SCREEN_HEIGHT 64 // La hauteur de l'écran, en pixels
+
+// Declaration d'un objet de type Adafruit_SSD1306
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  pinMode(bp, INPUT);
-  barriere.attach(3);
-  barriere.write(ferme);
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Affiche le texte "Hello world!"
+  display.println("Hello world!");
+  display.display(); 
 }
 
-void loop() {
-  int n = analogRead(LDR); // On lit la valeur de la LDR et on l'atribut à n
-  
-  if (digitalRead(bp) == HIGH && n < 250) { // Si le badge et la voiture sont présents 
-    barriere.write(ouvre);
-  }
-
-  else if (n > 250) {
-    delay(1000);
-    barriere.write(ferme);
-  }
-}
+void loop() {}
