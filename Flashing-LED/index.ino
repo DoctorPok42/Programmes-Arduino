55c5d1a964a449eb3af1b6c75df13780c765895a
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2022-01-22 22:59:08 +0100

    Create index.ino

---
+++ Flashing-LED/index.ino
@@ -0,0 +1,18 @@
+#define RED_LED 3
+
+void setup() {
+   // initialise les broches
+   pinMode(RED_LED, OUTPUT);
+}
+
+void loop() {
+   for(int i=0; i<=255; i++) {
+      analogWrite(RED_LED, i);
+      delay(10);
+   }
+
+   for(int i=255; i>=0; i--) {
+      analogWrite(RED_LED, i);
+      delay(10);
+   }
+}
